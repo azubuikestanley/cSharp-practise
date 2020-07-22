5e83606ec3e50317d633235ff9f628c1c19663dd
A-Stanley <astanley@uplandsoftware.com>
2020-07-22 16:32:17 +0100

    test console app commit

---
+++ ConsoleAppTestPractise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTestPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
