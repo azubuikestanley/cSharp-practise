71568daeb9435470a07ef4af26c0894e5c3a5315
A-Stanley <astanley@uplandsoftware.com>
2020-07-23 16:18:34 +0100

    continue practise

--- ConsoleAppTestPractise/Program.cs
+++ ConsoleAppTestPractise/Program.cs
@@ -7,6 +7,19 @@ namespace ConsoleAppTestPractise
         static void Main(string[] args)
         {   
             Console.WriteLine("Hello World!");
+
+            //replacing variable with placeholder
+            int x = 54;
+            double y = 30;
+            Console.WriteLine("x = {0}; y = {1}", x, y);
+
+            //getting users input
+            string yourName;
+            Console.WriteLine("What is your name");
+
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", yourName);
         }
     }
 }
