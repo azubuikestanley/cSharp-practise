680cc41f003c2145d8f46b37d9fd65667871a6d8
A-Stanley <astanley@uplandsoftware.com>
2020-08-31 19:46:59 +0100

    arrays and loop

--- ConsoleAppTestPractise/ArrayString.cs
+++ ConsoleAppTestPractise/ArrayString.cs
@@ -4,7 +4,101 @@ using System.Text;
 
 namespace ConsoleAppTestPractise
 {
-    class Class1
+    class ArrayString
     {
+        static void Main(string[] args)
+        {
+            //An array is a data structure that is used to store a collection of data, you can think of it as a collection of variables of the same type 
+            //we first instantiate an array to create it, eh below
+            int[] myArray = new int[5];  //we then assign values to individual element by using the index number
+            myArray[0] = 23;
+            //we can provide initial values to the array when it is declared by using curly brackets
+            string[] name = new string[3] { "John", "Mary", "Jessy" };
+            double[] price = new double[3] { 2.4, 4.5, 5.6 };
+            //we can omit the size declaration when the number of elements are provided in the curly braces
+            string[] names = new string[] { "John", "Mary", "Jessy" };
+            double[] prices = new double[] { 2.4, 4.5, 5.6 };
+            //we can even omit the new operator
+            string[] names2 = { "John", "Mary", "Jessy" };
+            double[] prices2 = { 2.4, 4.5, 5.6 };
+            //Array value should be provided in a comma separated list enclosed in {curly braces}
+
+            //using Arrays in Loops
+            int[] a = new int[10];
+            for (int k = 0; k < 10; k++)
+            {
+                a[k] = k * 2;
+            }
+            for (int k = 0; k < 10; k++)
+            {
+                Console.WriteLine(a[k]);
+            }
+
+
+            //foreach Loop
+            //code above can be rewritten as below with foreach loop
+            int[] b = new int[10];
+            for (int l = 0; l < 10; l++)
+            {
+                b[l] = l * 2;
+            }
+            foreach (int l in b)
+            {
+                Console.WriteLine(l);
+            }
+            //write a forach loop to calculate the sum of all the elements of an array
+            int[] arr = { 11, 34, 54, 467, 656 };
+            int sum = 0;
+
+            foreach (int c in arr)
+            {
+                sum += c;
+            }
+            Console.WriteLine(sum);
+
+
+            /*
+             * Multidimensional Arrays
+               A multidimensional array is declared as follows:
+               type[, , ...,] arrayName = new type[size1, size2, ..., sizeN];
+               The example below is a two dimensional 3x4 interger array
+               int[ , ] x = new int[3,4];
+               int[,] someNums = { { 2, 3 }, { 7, 5 }, { 4, 6 } };  //Array with three ows and two columns, nested curly braces are used to define values for each row.
+               To access an element of an array, provide both indexes, eg , someNums[2,0] will return the value 4 as it accesses the first column of the third row. 
+             */
+            //Let create a program that will display the values of the array in the form of  table.
+            int[,] someNums = { { 2, 3 }, { 7, 5 }, { 4, 6 } };
+            for (int m = 0; m  < 3; m++)
+            {
+                for (int n = 0; n < 2; n++)
+                {
+                    Console.WriteLine(someNums[m, n] + "");
+                }
+                Console.WriteLine();
+            }
+
+
+            /*
+             * Jagged Arrays
+             * This is an array whose element are arrays,, basically an array of arrays.
+             * example - int [][] jaggedArr = new int[3][];
+             */
+            int[][] jaggedArr = new int[][]
+            {
+                new int[] {1,8,2,7,9},
+                new int[] {2,4,6},
+                new int[] {33,42}
+            };
+            //we can access individual array element as shown below
+            int jA = jaggedArr[2][1]; //42 - access second element of the third array
+            //Note
+            /*A jagged array is an array of array, so an int[][] is an array of int[], each of which can beof different lengths and occupy their own block in memory 
+             *A multidimentional array (int[,]) is a single block of memory(essentially a matrix). It always has the same amount of columns for every row
+             */
+
+            
+            //Array properties 
+
+        }
     }
 }
--- ConsoleAppTestPractise/ClassObject.cs
+++ ConsoleAppTestPractise/ClassObject.cs
@@ -34,23 +34,164 @@ namespace ConsoleAppTestPractise
             /* We can access all public members of a class using the dot operator. Beside calling amethod, you can use the dot operator
              * to make an assignment when valid.
              * For example
-               class Dog 
-               {
-                public string name;
-                public int age;
-               }
-
-                static void Main(string[] arg)
-                {
-                    Dog bob = new Dog();
-                    bob.name = "Bobby";
-                    bob.age = 3;
-
-                   Console.WriteLine(bob.age); //output 3
-                   //see Method.cs file (187) for code reference to ClassObject (10)
-                }
-             * 
+                 //see Method.cs file (187) for code reference to ClassObject (10)
              */
+
+
+            /*Encapsulation - part of the meaning of this word is the idea of surrounding an entity, that is to protect it
+             * Encapsulation means combining members together within a class; it also means restricing access to the inner working of that class.
+             * This is implemented by using access modifiers, access modifier defines the scope and visibility of a class member.
+             * Encapsulation is also called information hiding
+             * this following access modifiers is supported in C# - public, private, protected, internal, protected internal
+             * public access modifier makes the member accessible from outside of the class
+             * private access modifier makes the member accessible only from within the class and hides them from the outside
+             * example
+                class BankAccount
+                {
+                    private double balance=0;
+                    public void Deposit(double n)
+                    {
+                        balance += n;
+                    }
+                    public void Withdraw(double n)
+                    {
+                        balance -= n;
+                    }
+                    public double GetBalance()
+                    {
+                     return balance;
+                    }
+                }
+
+                -encapsulation is used here to hide the balance member from outside the code. then we provided restricted access to it
+                using the public methods. The class data can only be read through the GetBalance method and modified only through the
+                Deposit and Withdraw methods. you cannot directly change the balance variable. You can only view its value using the public method. this helps
+                maintain data integrity. In summary, benefits of encapsulation are
+                -control the way data is accessed or modified
+                -Code is more flexible and easy to change with new requirements.
+                -Change one part of code without affecting other parts of code
+             */
+
+
+
+            /*Constructors
+             * A class constructor is a special member method of a class that is executed whenever a new object of that class is created
+             * A constructor has exactly the same name as its class, is public and does not have any return type
+             * example
+              class Person
+              {
+                 private int age;
+                 public Person()
+                 {
+                    Console.WriteLine("Hi there");
+                 }
+              }
+              when creation of an object of type Person, the constructor is automatically called
+              static void main(string[] args)
+              {
+                Person o = new Person();
+              }
+              This is useful in a number of situations, for example, when creating an object of type BankAccount, you could send an email
+              notification to the owner. The same functionality could be achieved using a separate public method. the advantage of the constructor
+              is that it is called automatically.
+
+              Constructor can be useful for setting initial values, for certain member variables. Default constructor has no parameters, However
+              when needed, parameters can be added to a constructor. this makes it possible to assign an initial value to an object when it's created.
+              example
+              class Person
+              {
+                private int age;
+                private string name;
+                public Person(string nm)
+                {
+                  name = nm;
+                }
+                public string getName()
+                {
+                    return name;
+                }
+              }
+              static void Main(string[] args)
+              {
+                 Person p = new Person("David");
+                 Console.WriteLine(p.getName());
+              }
+              when the object is created, we can pass a parameter that will be assigned to the name variable
+             * Note by constructor can be overloaded like any method by using different numbers of parameters
+             */
+
+
+            /*Properties
+             * we see that its a good practise to encapsulate members of a class and provide access to them only through public methods
+             * A property is a member thta provides a flexible mechanism to read, write or compute the value of a private field. Properties can be used as if
+             * they are public data members, but they actually include special methods called accessors.
+             * The accessor of a property contains the executable statements that help in getting (reading or computing) or setting (writing) a corresponding
+             * field. Accessor declarations can include a get accessor, a set accessor, or both.
+             *example
+                class Person
+                {
+                    private string name;
+                    public string Name
+                    {
+                        get {return  name; }
+                        set { name = value;}
+                    }
+                } //value is a special keyword, which represents the value we assign to a property using the set accessor.
+                //Name of a property can be nything we want but coding conventions dictate properties have the same name as the private field with a capital letter
+                
+            *Once the property is defined, we can use it to assign and read the private member
+                static void Main(string[] args)
+                {
+                    Person p = new Person();
+                    p.Name = "Bob";
+                    Console.WriteLine(p.Name);
+                }
+             * property is accessed by its name, just like any other public member of the class
+             *  
+             *  
+             *  Any access of a property can be omited. the code below creates a read only property
+                class Person
+                {
+                    private string name;
+                    public string Name
+                    {
+                        get { return name; }
+                    }
+             * A property can also be private, so it can be called only from within the class
+             * why use property? why not just declear the member variable public and access it directly. With properties you have the option
+             * to control the logic of accessing the variable.
+             * For example, we can check if the value of age is greater than 0, before assigning it to the variable
+                 class Person
+                 {
+                     private int age=0;
+                     public int Age
+                     {
+                        get { return age; }
+                        set {
+                            if (value > 0)
+                                age = value;
+                             }
+                       }
+                  }
+                  //we can have any custom logic with get and set accessors
+             * 
+             *  
+             *  Auto-Implemented Properties
+             *  public string Name { get; set; }
+             *  In the syntax above, we do not need to declear the private field name separately, Name here is called the
+             *  auto-implemented property, the private field is created automaticaly using this syntax. the example below for same code on line 131
+                  class Person 
+                  {
+                      public string Name { get; set; }
+                  }
+                  static void Main(string[] args)
+                  {
+                      Person p = new Person();
+                      p.Name = "Bob";
+                      Console.WriteLine(p.Name);
+             *  
+             */
+
         }
     }
 }
