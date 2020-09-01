4e418f15a110fc833d537988e546216204a84353
A-Stanley <astanley@uplandsoftware.com>
2020-09-01 17:59:50 +0100

    strings

--- ConsoleAppTestPractise/ArrayString.cs
+++ ConsoleAppTestPractise/ArrayString.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleAppTestPractise
@@ -91,13 +92,83 @@ namespace ConsoleAppTestPractise
             };
             //we can access individual array element as shown below
             int jA = jaggedArr[2][1]; //42 - access second element of the third array
-            //Note
+                                      //Note
             /*A jagged array is an array of array, so an int[][] is an array of int[], each of which can beof different lengths and occupy their own block in memory 
              *A multidimentional array (int[,]) is a single block of memory(essentially a matrix). It always has the same amount of columns for every row
              */
 
-            
-            //Array properties 
+
+            /*
+             * Array properties
+             * The array class in C# privides provides various properties and method to work with arrays.for example, the Length and Rank property
+             * return the number of elements and the number of dimensions of the array respectively.it can be access with dot syntax as seen below
+             */
+            int[] arry = { 3, 5, 8, 4 };
+            Console.WriteLine(arry.Length);
+            Console.WriteLine(arry.Rank);
+            //length property can be useful in for loops when we want to specify the number of times the loop should run
+            for(int k = 0; k< arry.Length; k++)
+            {
+                Console.WriteLine(arry[k]);
+            }
+            //Array method
+            //There are a number of methods available for arrays, Max returns the largest value, Min returns the smallest value, Sum returns the sum of all elements
+            int[] arys = { 3, 5, 8, 4 };
+            Console.WriteLine(arys.Max());
+            Console.WriteLine(arys.Min());
+            Console.WriteLine(arys.Sum());
+
+
+            /*
+             * Strings
+             * strings in C# are object, when you declare a string variable, you basically instantiate an object of type String.
+             * String objects suport a number of useful properties and methods:
+             * - Length returns the length of the string.
+             * - IndexOf(value) returns the index of the first occurence of the value within the string
+             * - Insert(index, value) inserts the value into the string starting from the specified index
+             * - Remove(index) removes all characters in the string after the specified index.
+             * - Replace(oldValue, newValue) replaces the specified value in the string
+             * - Substring(index, length) returns a substring of the specified length, starting from the specified index. If length is not specified,
+             *   the operation continues to the end of the string
+             * - Contains(value) returns true if the string contains the specified value.
+             * Examples below demonstrate each String member  
+             */
+            string ae = "some fun text";
+            //Length
+            Console.WriteLine(ae.Length); //output 13
+            //IndexOf
+            Console.WriteLine(ae.IndexOf("f")); //output 5
+            //Insert
+            ae = ae.Insert(0, "This is ");
+            Console.WriteLine(ae); //output "This is some fun text"
+            //Replace
+            ae = ae.Replace("text", "code");
+            Console.WriteLine(ae); //output "This is some fun code"
+            //Contains
+            if(ae.Contains("fun"))
+            {
+                Console.WriteLine("yea");  //output yea
+            }
+            //Remove
+            ae = ae.Remove(16);
+            Console.WriteLine(ae); //output "This is some fun"
+            //Substring
+            ae = ae.Substring(5);
+            Console.WriteLine(ae); //output "is some fun"
+            //we can also access characters of a string by its index, just like accessing element of an array
+            Console.WriteLine(ae[6]); //output is e cos ae = "is some fun" now
+
+            //let's create a progeam that will take a string, replace all occurences of the word "dog" with "cat" and output the first sentence only
+            string test = "This is some test about a dog. the word dog appears in the text a number of times. this is the end.";
+
+            test = test.Replace("dog", "cat");
+            test = test.Substring(0, test.IndexOf("."));
+            Console.WriteLine(test);//output - This is some test about a cat.
+
+
+
+
+
 
         }
     }
