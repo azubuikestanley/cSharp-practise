7300dec59f4aa5eeb729b97e0e052c58bae0405e
A-Stanley <astanley@uplandsoftware.com>
2020-09-07 15:04:35 +0100

    Generic classes

--- ConsoleAppTestPractise/Generics.cs
+++ ConsoleAppTestPractise/Generics.cs
@@ -16,6 +16,26 @@ namespace ConsoleAppTestPractise
             b = temp;
         }
 
+        //Generic class - Stack
+        class Stack<T>
+        {
+            int index = 0;
+            T[] innerArray = new T[100];
+            public void Push(T item)
+            {
+                innerArray[index++] = item;
+            }
+            public T Pop()
+            {
+                return innerArray[--index];
+            }
+            public T Get(int k)
+            {
+                return innerArray[k];
+            }
+        }
+
+
 
         static void Main(string[] args)
         {
@@ -66,8 +86,29 @@ namespace ConsoleAppTestPractise
 
 
 
-
-
+            /* Generic Classes - 
+             * generic types can also be used with classes. The most common use for generic classes is with collections of items, where operations such as 
+             * adding and removing items from the collection are performed in basically the same way regardless of the type of data being stored.  One type
+             * of collection is called a stack. Items are "pushed", or added to the collection, and "popped", or removed from the collection. A stack is 
+             * sometimes called a Last in First Out(LIFO) data structure.
+             * for example - line(20)
+             * 
+             * The generic class stores elements in an array. As you can see, the generic type T is used as the type of the array, the parameter
+             * type for the Push method, and the return type for the Pop and Get methods.
+             * Now we can create objects of our generic class as below
+             */
+            Stack<int> IntStack = new Stack<int>();
+            Stack<string> strStack = new Stack<string>();
+            //Stack<Person> PersonStack = new Stack<Person>()
+            /*We can also use the generic class with custom types, such as the custom defined Person type.
+             * In a generic class we do not need to define the generic type for its methods, because the generic type is already defined on the class level.
+             * Generic class methods are called the same as for any other object.
+             */
+            Stack<int> intStack2 = new Stack<int>();
+            intStack2.Push(3);
+            intStack2.Push(6);
+            intStack2.Push(7);
+            Console.WriteLine(intStack2.Get(1));
 
 
         }
