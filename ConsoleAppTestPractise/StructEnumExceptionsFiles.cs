d23f21fa3791f0e75c3b1fc9532bc0b0b90a9481
A-Stanley <astanley@uplandsoftware.com>
2020-09-07 13:24:33 +0100

    Exception

--- ConsoleAppTestPractise/StructEnumExceptionsFiles.cs
+++ ConsoleAppTestPractise/StructEnumExceptionsFiles.cs
@@ -98,11 +98,49 @@ namespace ConsoleAppTestPractise
             }
 
 
-            /*
+            /*Exception handling
+             * An exception is a problem that occurs during program execution. Exceptions cause abnormal termination of the program.
+             * An exception can occur for many different reasons. Some examples
+             * -A user has  entered invalid data.
+             * -A file that needs to be opened cannot be found.
+             * -A network connection has been lost in the middle of communications.
+             * -Insufficient memory and other issues related to physical resources
+             * For example, the code below will produce an exception when run because we request an index which does not exist
+             * int[] arr = new int[] {4, 5, 6, 8};
+             * Console.WriteLine(arr[8]);
+             * 
+             * As you can see, exceptions are caused by user error, programmer error, or physical resource issues. However, a weel-written program 
+             * should handle all possible exceptions.
              * 
              * 
+             * Handling Exceptions
+             * C# provides a flexible mechanism called the try-catch statement to handle exceptions so that a program won't crash when an error occurs.
+             * The try and catch blocks are used similar to as seen below
              */
-
+             try
+             {
+                int[] arr = { 3, 5, 7 };
+                Console.WriteLine(arr[8]);
+             }
+             catch(Exception e)
+             {
+                Console.WriteLine("An error occurred");
+             }
+            /*The code that might generate an exception is placed in the try block. If an exception occurs, the catch blocks is executed without stopping
+             * the program.
+             * The type of exception you want to catch appears in parentheses following the keyword catch.
+             * We use the general Exception type to handle all kind of exceptions. We can also use the exception object e to access the exception details, 
+             * such as the original error message (e.Message) as seen below
+             */
+            try
+            {
+                int[] arr = new int[]{ 3, 5, 7 };
+                Console.WriteLine(arr[8]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); //Output - Index was outside the bounds of the array
+            }
 
 
         }
